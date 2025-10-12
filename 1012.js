ad634a767ccc8cc7d645d43af012d88e333b0d12
Michael Moura <113400472+Dev-Moura@users.noreply.github.com>
2025-10-12 12:02:40 -0300

    solve 1011 1012

---
+++ 1012.js
@@ -0,0 +1,32 @@
+/*
+
+Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
+a) a área do triângulo retângulo que tem A por base e C por altura.
+b) a área do círculo de raio C. (pi = 3.14159)
+c) a área do trapézio que tem A e B por bases e C por altura.
+d) a área do quadrado que tem lado B.
+e) a área do retângulo que tem lados A e B.
+Entrada
+
+O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+Saída
+
+O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima, sempre com mensagem correspondente e um espaço entre os dois pontos e o valor. O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+
+
+*/
+
+let values = lines.shift().trim().split(" ");
+let [a, b, c] = [parseFloat(values.shift()), parseFloat(values.shift()), parseFloat(values.shift())];
+
+let T = (a * c) / 2;
+let C = 3.14159 * Math.pow(c, 2);
+let TP = ((a + b) * c) / 2;
+let Q = b * b;
+let R = a * b;
+
+console.log("TRIANGULO: " + T.toFixed(3));
+console.log("CIRCULO: " + C.toFixed(3));
+console.log("TRAPEZIO: " + TP.toFixed(3));
+console.log("QUADRADO: " + Q.toFixed(3));
+console.log("RETANGULO: " + R.toFixed(3));
